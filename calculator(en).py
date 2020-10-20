ace2b2ef6603be5ccb112736a740d42d9bdf60ae
melih <43184082+melihozseker@users.noreply.github.com>
2020-10-20 23:49:14 +0300

    Add files via upload

---
+++ calculator(en).py
@@ -0,0 +1,30 @@
+#this will work
+# Melih Özşeker
+
+process=input("enter the process:")
+
+number1=int(input("Number1:"))
+number2=int(input("Number2:"))
+
+if process=="+":
+   result=int(number1)+int(number2)
+   print("Result:",str(result))
+
+elif process=="-":
+    result=int(number1)-int(number2)
+    print("Result:",str(result))
+
+elif process=="*":
+    result=int(number1)-int(number2)
+    print("Result:",str(result))
+
+elif process=="/":
+     result=int(number1)-int(number2)
+     print("Result:",str(result))
+
+
+
+
+
+
+
