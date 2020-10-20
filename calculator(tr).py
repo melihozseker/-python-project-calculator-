e6e9facad3195f3f9ce04dc92ba41a43ee146ae1
melih <43184082+melihozseker@users.noreply.github.com>
2020-10-20 23:41:56 +0300

    Add files via upload

---
+++ calculator(tr).py
@@ -0,0 +1,30 @@
+#this will work
+# Melih Özşeker
+
+islem=input("islemi giriniz:")
+
+sayi1=int(input("Sayi1:"))
+sayi2=int(input("Sayi2:"))
+
+if islem=="+":
+   sonuc=int(sayi1)+int(sayi2)
+   print("Sonuc:",str(sonuc))
+
+elif islem=="-":
+    sonuc=int(sayi1)-int(sayi2)
+    print("Sonuc:",str(sonuc))
+
+elif islem=="*":
+    sonuc=int(sayi1)-int(sayi2)
+    print("Sonuc:",str(sonuc))
+
+elif islem=="/":
+     sonuc=int(sayi1)-int(sayi2)
+     print("Sonuc:",str(sonuc))
+
+
+
+
+
+
+
